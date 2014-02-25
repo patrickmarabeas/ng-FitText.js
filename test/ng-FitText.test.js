dbcbaf1ca3b00f1ed6fde3745348ebee797acca8
Patrick Marabeas <patrick@pulse-dev.com.au>
2014-02-25 23:35:22 +1100

    Unit tests can now be performed without creating and appending a DOM element

--- test/ng-FitText.test.js
+++ test/ng-FitText.test.js
@@ -6,34 +6,28 @@ describe( 'FitText directive with a width of 500px', function() {
 
 	beforeEach( module( 'ngFitText' ));
 	beforeEach( inject( function( $compile, $rootScope ) {
+		$scope = $rootScope;
+		element = angular.element("<div data-fittext>Sample Text</div>");
+		$compile(element)($rootScope);
 
-		element = document.createElement('div');
-		element.innerHTML = 'FitText';
-		element.setAttribute('data-fittext','');
-		element.setAttribute('style','width:500px');
-		document.body.appendChild(element);
+		element.scope().elementWidth = 500;
 	}));
 
 	describe( 'font-size with default compressor value of 1', function() {
-
-		beforeEach( inject( function( $compile, $rootScope ) {
-			element = $compile( element )($rootScope);
-		}));
-
 		it( 'should equal 50px', function() {
+			element.scope().compressor = 1;
+			element.scope().resizer();
+
 			expect( element.scope().fontSize )
 				.toEqual( '50px' );
 		});
 	});
 
 	describe( 'font-size with custom compressor value of .25', function() {
-
-		beforeEach( inject( function( $compile, $rootScope ) {
-			element.setAttribute('data-fittext','.25');
-			element = $compile( element )($rootScope);
-		}));
-
 		it( 'should equal 200px', function() {
+			element.scope().compressor = .25;
+			element.scope().resizer();
+
 			expect( element.scope().fontSize )
 				.toEqual( '200px' );
 		});
@@ -42,26 +36,20 @@ describe( 'FitText directive with a width of 500px', function() {
 	describe( 'font-size with min values', function() {
 
 		describe( 'font-size with min value of 80', function() {
-
-			beforeEach( inject( function( $compile, $rootScope ) {
-				element.setAttribute('data-fittext-min','80');
-				element = $compile( element )($rootScope);
-			}));
-
 			it( 'should equal 80px', function() {
+				element.scope().minFontSize = 80;
+				element.scope().resizer();
+
 				expect( element.scope().fontSize )
 					.toEqual( '80px' );
 			});
 		});
 
 		describe( 'font-size with min value of 10', function() {
-
-			beforeEach( inject( function( $compile, $rootScope ) {
-				element.setAttribute('data-fittext-min','10');
-				element = $compile( element )($rootScope);
-			}));
-
 			it( 'should equal 50px', function() {
+				element.scope().minFontSize = 10;
+				element.scope().resizer();
+
 				expect( element.scope().fontSize )
 					.toEqual( '50px' );
 			});
@@ -71,26 +59,20 @@ describe( 'FitText directive with a width of 500px', function() {
 	describe( 'font-size with max values', function() {
 
 		describe( 'font-size with max value of 10', function() {
-
-			beforeEach( inject( function( $compile, $rootScope ) {
-				element.setAttribute('data-fittext-max','10');
-				element = $compile( element )($rootScope);
-			}));
-
 			it( 'should equal 10px', function() {
+				element.scope().maxFontSize = 10;
+				element.scope().resizer();
+
 				expect( element.scope().fontSize )
 					.toEqual( '10px' );
 			});
 		});
 
 		describe( 'font-size with max value of 100', function() {
-
-			beforeEach( inject( function( $compile, $rootScope ) {
-				element.setAttribute('data-fittext-max','100');
-				element = $compile( element )($rootScope);
-			}));
-
 			it( 'should equal 50px', function() {
+				element.scope().maxFontSize = 100;
+				element.scope().resizer();
+
 				expect( element.scope().fontSize )
 					.toEqual( '50px' );
 			});
