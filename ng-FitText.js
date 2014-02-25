a30fb6a5de785d0bd39b52a46ad6350a237163f3
Patrick Marabeas <patrick@pulse-dev.com.au>
2014-02-25 23:34:25 +1100

    Version to v2.0.0 rewritten so it's even more Angular

--- ng-FitText.js
+++ ng-FitText.js
@@ -1,57 +1,51 @@
-/* ng-FitText.js v1.0.4
+/* ng-FitText.js v2.0.0
  * https://github.com/patrickmarabeas/ng-FitText.js
  *
  * Original jQuery project: https://github.com/davatron5000/FitText.js
  *
- * Copyright 2013, Patrick Marabeas http://marabeas.io
+ * Copyright 2014, Patrick Marabeas http://marabeas.io
  * Released under the MIT license
  * http://opensource.org/licenses/mit-license.php
  *
- * Date: 14/02/2014
+ * Date: 25/02/2014
  */
 
 'use strict';
 
 angular.module( 'ngFitText', [] )
-.directive( 'fittext', [ function() {
-	return {
-		restrict: 'A',
-		scope: true,
-//		If the element is safe to transclude / template the following block can be used:
-//		transclude: true,
-//		replace: true,
-//		template: function( element, attrs ) {
-//			var tag = element[0].nodeName;
-//			return "<"+tag+" data-ng-transclude data-ng-style='{fontSize:fontSize}'></"+tag+">";
-//		},
-		link: function( scope, element, attrs ) {
-			scope.compressor = attrs.fittext || 1;
-			scope.minFontSize = attrs.fittextMin || Number.NEGATIVE_INFINITY;
-			scope.maxFontSize = attrs.fittextMax || Number.POSITIVE_INFINITY;
+	.directive( 'fittext', [ function() {
+		return {
+			restrict: 'A',
+			scope: true,
+			transclude: true,
+			replace: true,
+			template: function( element, attrs ) {
+				var tag = element[0].nodeName;
+				return "<"+tag+" data-ng-transclude data-ng-style='{fontSize:fontSize}'></"+tag+">";
+			},
+			link: function( scope, element, attrs ) {
+				scope.compressor = attrs.fittext || 1;
+				scope.minFontSize = attrs.fittextMin || Number.NEGATIVE_INFINITY;
+				scope.maxFontSize = attrs.fittextMax || Number.POSITIVE_INFINITY;
+				scope.elementWidth = element[0].offsetWidth;
 
-			var resizer = function() {
-				scope.fontSize = Math.max(
-					Math.min(
-						element[0].offsetWidth / ( scope.compressor * 10 ),
-						parseFloat( scope.maxFontSize )
-					),
-					parseFloat( scope.minFontSize )
-				) + 'px';
+				( scope.resizer = function() {
+					scope.fontSize = Math.max(
+						Math.min(
+							scope.elementWidth / ( scope.compressor * 10 ),
+							parseFloat( scope.maxFontSize )
+						),
+						parseFloat( scope.minFontSize )
+					) + 'px';
 
-//				The following line can be replaced with the above block of commented code for a more angular approach if
-//				the element won't have multiple directives calling transclusion or templates on it, or the DOM isn't
-//				being manipulated. Since I can't assume what this directive will be used in combination with - this
-//				is the sure fire method:
-				element[0].style.fontSize = scope.fontSize;
-			}; resizer(); //required for tests to pass without Chrome test window being minimised (resource starved - i.e. document.ready completes)
+					if( !scope.$$phase ) scope.$digest();
 
-			angular.element( document ).ready( function() {
-				resizer();
-			});
+				})();
 
-			angular.element( window ).bind( 'resize', function() {
-				scope.$apply( resizer );
-			});
+				angular.element( window ).bind( 'resize', function() {
+					scope.elementWidth = element[0].offsetWidth;
+					scope.resizer();
+				});
+			}
 		}
-	}
-}]);
+	}]);
