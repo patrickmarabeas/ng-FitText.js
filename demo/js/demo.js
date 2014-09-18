55f688d149c863024c564623fdfe3192122b7c9f
Patrick Marabeas <patrick@marabeas.io>
2014-09-18 20:21:38 +1000

    Update demo and dev tooling for v3.0.0

---
+++ demo/js/demo.js
@@ -0,0 +1,19 @@
+;(function(window, document, angular, undefined) {
+
+  'use strict';
+
+  angular.module( 'demo', ['ngFitText'] )
+    .config(['fitTextConfigProvider', function(fitTextConfigProvider) {
+      fitTextConfigProvider.config = {
+//        debounce: function(a,b,c) {
+//          var d;return function(){var e=this,f=arguments;clearTimeout(d),d=setTimeout(function(){d=null,c||a.apply(e,f)},b),c&&!d&&a.apply(e,f)}
+//        },
+//        delay: 1000
+      };
+    }])
+    .controller('MainController', ['$scope', function($scope) {
+      $scope.dyn = 'FitText';
+    }]);
+
+})(window, document, angular);
+
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp          = require('gulp')
+  , sass          = require('gulp-sass');
+
+gulp.task('scripts', function() {
+  gulp.src('./lib/angular/angular.min.js')
+    .pipe(gulp.dest('./demo/js'));
+});
+
+gulp.task('styles', function() {
+  gulp.src('./src/styles/main.scss')
+    .on('data', function(file) {
+      // https://github.com/dlmanning/gulp-sass/issues/28#issuecomment-43951089
+      var path = require('path');
+      if (process.platform === 'win32') {
+        file.path = path.relative('.', file.path);
+        file.path = file.path.replace(/\\/g, '/');
+      }
+    })
+    .pipe(sass({ errLogToConsole: true })) // error handling still breaks in conjunction with sourcemaps
+    .pipe(gulp.dest('./demo/styles'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*.scss', ['styles']);
+});
+
+gulp.task('default',['styles', 'scripts']);
