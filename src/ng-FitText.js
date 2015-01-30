e1e280e24d2852470aa5d56b03230fe9d463ddf7
Patrick Marabeas <patrick@marabeas.io>
2015-01-30 17:24:19 +1100

    Add ability to specify new lines

--- src/ng-FitText.js
+++ src/ng-FitText.js
@@ -1,10 +1,9 @@
-/* ng-FitText.js v3.2.0
+/* ng-FitText.js v3.3.0
  * https://github.com/patrickmarabeas/ng-FitText.js
  *
  * Original jQuery project: https://github.com/davatron5000/FitText.js
- * Includes use of Underscore's debounce function
  *
- * Copyright 2014, Patrick Marabeas http://marabeas.io
+ * Copyright 2015, Patrick Marabeas http://marabeas.io
  * Released under the MIT license
  * http://opensource.org/licenses/mit-license.php
  *
@@ -37,12 +36,13 @@
           var parent = element.parent();
           var compressor = attrs.fittext || 1;
           var loadDelay = attrs.fittextLoadDelay || config.loadDelay;
+          var nl = element[0].querySelectorAll('[fittext-nl],[data-fittext-nl]').length || 1;
           var minFontSize = attrs.fittextMin || config.min || Number.NEGATIVE_INFINITY;
           var maxFontSize = attrs.fittextMax || config.max || Number.POSITIVE_INFINITY;
 
           var resizer = function() {
             element[0].style.fontSize = '10px';
-            var ratio = element[0].offsetHeight / element[0].offsetWidth;
+            var ratio = element[0].offsetHeight / element[0].offsetWidth / nl;
             element[0].style.fontSize = Math.max(
               Math.min((parent[0].offsetWidth - 6) * ratio * compressor,
                 parseFloat(maxFontSize)
